877def665a3611a4343e76e65029acfaca369ed5
Civiled <civiled@usa.com>
2020-12-29 16:54:23 -0800

    Added build script
    For now if you're trying to use the program, you need to edit the script
    with your account credentials to access the amazon services

---
+++ build.sh
@@ -0,0 +1,8 @@
+# Replace your ACCESS KEY ID here
+export AWS_ACCESS_KEY_ID=
+# Replace your SECRET ACCESS KEY here
+export AWS_SECRET_ACCESS_KEY=
+export AWS_PRODUCT_REGION=JP
+export AWS_ASSOCIATE_TAG=ngsio-22
+
+go build
