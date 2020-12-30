51293c5a4e437deb8f97a2a5a7b742b2466abe15
Civiled <civiled@usa.com>
2020-12-29 16:55:22 -0800

    Added action ID for registering actions and finding actions in a non hacky way with alias support

---
+++ actions/action_id.go
@@ -0,0 +1,33 @@
+package actions
+
+import (
+	"fmt"
+	"strings"
+)
+
+var RegisteredActions = map[string]func() Action{}
+
+// Get the proper action based on it's ID
+func FromID(ID string) (error, Action) {
+	for id, action := range RegisteredActions {
+		if strings.ToLower(ID) == id {
+			return nil, action()
+		}
+	}
+
+	return fmt.Errorf("couldn't find action under the id %s", ID), nil
+}
+
+func init() {
+	// Push all of the actions into a list
+	actions := []func() Action{
+		func() Action { return &ProductLookup{} },
+	}
+
+	// Register all actions from our list into a map containing aliases too
+	for _, action := range actions {
+		for _, aliase := range action().ID() {
+			RegisteredActions[aliase] = action
+		}
+	}
+}
